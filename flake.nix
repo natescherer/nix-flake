4b31686836dae9a59449a80c72e63af04ad02552
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-05-21 03:18:27 -0400

    feat: add initial flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{self, nixpkgs, flake-utils, home-manager, ...}:
+    flake-utils.lib.eachDefaultSystem (system: 
+    let 
+        pkgs = import nixpkgs { inherit system; };
+    in {
+        legacyPackages = {
+            homeConfigurations = {
+                nixtools = home-manager.lib.homeManagerConfiguration {
+                    inherit pkgs;
+                    modules = [ ./home.nix ];
+                };
+            };
+        };
+    });
+}
---
+++ home.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: {
+    home.username = "nate";
+    home.homeDirectory = "/Users/nate";
+    home.packages = [
+        pkgs.nixfmt-rfc-style
+        pkgs.cowsay
+        pkgs.nixd
+    ];
+    home.stateVersion = "24.11";
+    programs.home-manager.enable = true;
+}
